3202be7c6a8ea36e06beb5b274e906e458b55fe7
Itay Wiseman <itayw@traiana.com>
2016-08-05 16:50:36 -0400

    react-skeleton

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ListA = require('./components/List.jsx');
+
+ReactDOM.render(<ListA/>, document.getElementById('ingredients'));
